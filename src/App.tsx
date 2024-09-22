a7b01c505140b94746f18e5c4eb5794dfef03db3
madan-stha <madan@scieverinc.com>
2024-09-22 15:40:11 +0545

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GanttChart from "./GanttChart";
 
-function App() {
+// Define the task structure
+const tasks = [
+  {
+    taskId: "TAsk1",
+    name: "Daily Stand-up Meeting",
+    startDate: new Date("2024-09-02"),
+    endDate: new Date("2024-09-04"),
+    color: "rgb(81, 91, 82)",
+  },
+  {
+    taskId: "TAsk2",
+    name: "Code Review for Feature X",
+    startDate: new Date("2024-09-04"),
+    endDate: new Date("2024-09-06"),
+    color: "rgb(227, 0, 0)",
+  },
+  {
+    taskId: "TAsk31",
+    name: "Add new button",
+    startDate: new Date("2024-09-05"),
+    endDate: new Date("2024-09-09"),
+    color: "rgb(147, 167, 149)",
+  },
+  {
+    taskId: "TAsk3",
+    name: "Deploy New Version to Production",
+    startDate: new Date("2024-09-09"),
+    endDate: new Date("2024-09-11"),
+    color: "rgb(255, 122, 0)",
+  },
+  {
+    taskId: "TAsk4",
+    name: "Client Meeting for Project Aplha",
+    startDate: new Date("2024-09-11"),
+    endDate: new Date("2024-09-13"),
+    color: "rgb(255, 122, 0)",
+  },
+  {
+    taskId: "TAsk5",
+    name: "Upgrade version of libraries",
+    startDate: new Date("2024-09-16"),
+    endDate: new Date("2024-09-16"),
+    color: "rgb(128, 128, 128)",
+  },
+  {
+    taskId: "TAsk6",
+    name: "Change color of texts",
+    startDate: new Date("2024-09-15"),
+    endDate: new Date("2024-09-17"),
+    color: "rgb(3, 192, 60)",
+  },
+  {
+    taskId: "TAsk7",
+    name: "Delivery and billing address soft delete issue",
+    startDate: new Date("2024-09-17"),
+    endDate: new Date("2024-09-19"),
+    color: "rgb(255, 122, 0)",
+  },
+  {
+    taskId: "TAsk8",
+    name: "Add aria-label in Button, link, menuItem and input fields",
+    startDate: new Date("2024-09-12"),
+    endDate: new Date("2024-09-16"),
+    color: "rgb(3, 192, 60)",
+  },
+  {
+    taskId: "TAsk9",
+    name: "The status in the downloaded excel report and system does not match. entered by Anushka",
+    startDate: new Date("2024-09-16"),
+    endDate: new Date("2024-09-18"),
+    color: "rgb(255, 165, 0)",
+  },
+  {
+    taskId: "TAsk10",
+    name: " Use WebP instead of JPG, PNG, etc.",
+    startDate: new Date("2024-09-17"),
+    endDate: new Date("2024-09-20"),
+    color: "rgb(3, 192, 60)",
+  },
+  {
+    taskId: "TAsk11",
+    name: "Security Audit",
+    startDate: new Date("2024-09-19"),
+    endDate: new Date("2024-09-20"),
+    color: "rgb(128, 0, 128)",
+  },
+  {
+    taskId: "TAsk12",
+    name: "Team Building Workshop",
+    startDate: new Date("2024-09-28"),
+    endDate: new Date("2024-10-01"),
+    color: "rgb(0, 128, 0)",
+  },
+  {
+    taskId: "TAsk13",
+    name: "Quarterly Performance Review",
+    startDate: new Date("2024-10-04"),
+    endDate: new Date("2024-10-06"),
+    color: "rgb(255, 165, 0)",
+  },
+  {
+    taskId: "TAsk14",
+    name: "On-call Duty Rotation",
+    startDate: new Date("2024-10-09"),
+    endDate: new Date("2024-10-11"),
+    color: "rgb(128, 128, 128)",
+  },
+  {
+    taskId: "TAsk15",
+    name: "Technology Conference Attendance",
+    startDate: new Date("2024-10-16"),
+    endDate: new Date("2024-10-18"),
+    color: "rgb(0, 0, 128)",
+  },
+  {
+    taskId: "TAsk16",
+    name: "Database Migration",
+    startDate: new Date("2024-10-21"),
+    endDate: new Date("2024-10-23"),
+    color: "rgb(255, 255, 0)",
+  },
+  {
+    taskId: "TAsk17",
+    name: "Product Launch Preparation",
+    startDate: new Date("2024-10-26"),
+    endDate: new Date("2024-10-28"),
+    color: "rgb(255, 255, 255)",
+  },
+  {
+    taskId: "TAsk18",
+    name: "Holiday Season Support",
+    startDate: new Date("2024-10-31"),
+    endDate: new Date("2024-11-02"),
+    color: "rgb(0, 255, 255)",
+  },
+  {
+    taskId: "TAsk19",
+    name: "New Hire Onboarding",
+    startDate: new Date("2024-11-06"),
+    endDate: new Date("2024-11-08"),
+    color: "rgb(255, 0, 255)",
+  },
+];
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gantt Chart Example</h1>
+      <GanttChart tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/GanttChart.tsx
@@ -0,0 +1,213 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+
+interface Task {
+  taskId: string;
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  color: string;
+}
+
+interface GanttChartProps {
+  tasks: Task[];
+}
+
+const GanttChart: React.FC<GanttChartProps> = ({ tasks }) => {
+  const svgRef = useRef<SVGSVGElement | null>(null);
+  const [currentWeek, setCurrentWeek] = useState<Date>(new Date(2024, 8, 15));
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (!tasks || tasks.length === 0) return;
+
+    const svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove();
+
+    const margin = { top: 60, right: 40, bottom: 40, left: 200 };
+    const width = 1800 - margin.left - margin.right;
+    const barHeight = 50;
+    const totalBars = tasks.length;
+
+    const dynamicHeight = totalBars * barHeight;
+    const height = dynamicHeight + margin.top + margin.bottom;
+
+    const startOfWeek = new Date(currentWeek);
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 5);
+
+    const filteredTasks = tasks.filter(
+      (task) =>
+        (task.startDate >= startOfWeek && task.startDate <= endOfWeek) ||
+        (task.endDate >= startOfWeek && task.endDate <= endOfWeek) ||
+        (task.startDate < startOfWeek && task.endDate > endOfWeek)
+    );
+
+    const taskIds = filteredTasks.map((task) => task.taskId);
+
+    const x = d3.scaleTime().domain([startOfWeek, endOfWeek]).range([0, width]);
+
+    const y = d3
+      .scaleBand()
+      .domain(taskIds)
+      .range([0, filteredTasks.length * barHeight + 25])
+      .padding(0);
+
+    const xAxis = d3
+      .axisTop(x)
+      .ticks(d3.timeDay.every(1))
+      .tickFormat(d3.timeFormat("%a %d %b") as any);
+
+    const yAxis = d3.axisLeft(y);
+
+    const chartGroup = svg
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const xAxisGroup = chartGroup.append("g").call(xAxis as any);
+
+    const yAxisGroup = chartGroup.append("g").call(yAxis);
+
+    const clipDate = (date: Date, start: Date, end: Date) => {
+      if (date < start) return start;
+      if (date > end) return end;
+      return date;
+    };
+
+    xAxisGroup
+      .selectAll(".tick text")
+      .style("font-size", "14px")
+      .style("font-weight", "bold");
+
+    yAxisGroup
+      .selectAll(".tick text")
+      .style("font-size", "14px")
+      .style("font-weight", "bold");
+
+    xAxisGroup
+      .select(".domain")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", "5,5");
+
+    yAxisGroup
+      .select(".domain")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", "5,5");
+
+    const bars = chartGroup
+      .selectAll("rect")
+      .data(filteredTasks)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => {
+        const clippedStartDate = clipDate(d.startDate, startOfWeek, endOfWeek);
+        return x(d3.timeDay.floor(clippedStartDate));
+      })
+      .attr("y", (d) => y(d.taskId)!)
+      .attr("width", (d) => {
+        const clippedStartDate = clipDate(d.startDate, startOfWeek, endOfWeek);
+        const clippedEndDate = clipDate(d.endDate, startOfWeek, endOfWeek);
+        return (
+          x(d3.timeDay.ceil(clippedEndDate)) -
+          x(d3.timeDay.floor(clippedStartDate))
+        );
+      })
+      .attr("height", barHeight)
+      .attr("fill", (d) => d.color)
+      .attr("stroke-width", 1)
+      .attr("rx", 15)
+      .attr("ry", 15);
+
+    chartGroup
+      .selectAll("text.task-label")
+      .data(filteredTasks)
+      .enter()
+      .append("text")
+      .attr("x", (d) => {
+        const clippedStartDate = clipDate(d.startDate, startOfWeek, endOfWeek);
+        return x(d3.timeDay.floor(clippedStartDate)) + 5;
+      })
+      .attr("y", (d) => y(d.taskId)! + y.bandwidth() / 2)
+      .attr("dy", ".35em")
+      .text((d) => d.name)
+      .style("fill", "#fff")
+      .style("font-size", "18px")
+      .style("font-weight", "bold")
+      .style("text-anchor", "start");
+
+    bars
+      .on("mouseover", function (event, d) {
+        d3.select(tooltipRef.current)
+          .style("opacity", 1)
+          .html(
+            `<strong>Task:</strong> ${d.name}<br>
+            <strong>Start:</strong> ${d3.timeFormat("%Y-%m-%d")(
+              d.startDate
+            )}<br>
+            <strong>End:</strong> ${d3.timeFormat("%Y-%m-%d")(d.endDate)}`
+          )
+          .style("left", `${event.pageX + 10}px`)
+          .style("top", `${event.pageY - 28}px`);
+      })
+      .on("mousemove", function (event) {
+        d3.select(tooltipRef.current)
+          .style("left", `${event.pageX + 10}px`)
+          .style("top", `${event.pageY - 28}px`);
+      })
+      .on("mouseout", function () {
+        d3.select(tooltipRef.current).style("opacity", 0);
+      });
+
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("text-align", "left")
+      .style("padding", "5px")
+      .style("background", "#333")
+      .style("color", "#fff")
+      .style("border-radius", "12px")
+      .style("opacity", 0);
+
+    tooltipRef.current = tooltip.node() as HTMLDivElement;
+  }, [tasks, currentWeek]);
+
+  const showPreviousWeek = () => {
+    setCurrentWeek((prevWeek) => {
+      const newDate = new Date(prevWeek);
+      newDate.setDate(prevWeek.getDate() - 7);
+      return newDate;
+    });
+  };
+
+  const showNextWeek = () => {
+    setCurrentWeek((prevWeek) => {
+      const newDate = new Date(prevWeek);
+      newDate.setDate(prevWeek.getDate() + 7);
+      return newDate;
+    });
+  };
+
+  const showCurrentWeek = () => {
+    setCurrentWeek(new Date(2024, 8, 15));
+  };
+
+  return (
+    <div>
+      <div style={{ marginLeft: "200px" }}>
+        <button onClick={showPreviousWeek}>Previous Sprint</button>
+        <button onClick={showCurrentWeek}>
+          Current Sprint (Sep 15-20, 2024)
+        </button>
+        <button onClick={showNextWeek}>Next Sprint</button>
+      </div>
+      <svg ref={svgRef} />
+    </div>
+  );
+};
+
+export default GanttChart;
